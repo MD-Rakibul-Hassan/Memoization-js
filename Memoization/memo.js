8e6a94dd9b92e9a5809d114b752bcaa3d0e965f0
Rakibul-hassan <mdrakibulhassan2425@gmail.com>
2024-04-15 12:43:15 +0600

    add final code

---
+++ Memoization/memo.js
@@ -0,0 +1,25 @@
+
+function sumOfLoop (n) {
+    return n + 10
+}
+
+
+// The memo is dynamic function for memoize any value 
+const memo = (fn) => {
+    // This is my cache object 
+    const cache = {};
+    return (val) => {
+        if(cache[val]) {
+            console.log("form catch")
+            return cache[val]
+        }else {
+            const result = sumOfLoop(val);
+            cache[val] = result;
+            return result;
+        }
+    }
+}
+const result = memo(sumOfLoop);
+
+console.log(result(20))
+console.log(result(20))
